105f7cd56f1d108c73afc5261f847991ca0822d6
ebrennan <ember.brennan@optum.com>
2021-03-25 11:05:37 +0000

    Adding more unit tests

--- tests/dream_team_tests.py
+++ tests/dream_team_tests.py
@@ -5,6 +5,11 @@ import wait_response
 
 
 class DreamTeamTests(unittest.TestCase):
+    test_goalie = {'gw_points': 1, 'id': 1, 'name': 'Test Goalie', 'pos': 1, 'price': 1}
+    test_defender = {'gw_points': 1, 'id': 1, 'name': 'Test Defender', 'pos': 2, 'price': 1}
+    test_midfielder = {'gw_points': 1, 'id': 1, 'name': 'Test Midfielder', 'pos': 3, 'price': 1}
+    test_striker = {'gw_points': 1, 'id': 1, 'name': 'Test Striker', 'pos': 4, 'price': 1}
+    test_player = {'gw_points': 1, 'id': 1, 'name': 'Test Player', 'pos': 99, 'price': 1}
 
     def test_initial_team_build(self):
         self.assertEqual(dream_team.dream_team, [])
@@ -13,10 +18,13 @@ class DreamTeamTests(unittest.TestCase):
 
     def test_too_many_goalies(self):
         tv_instance = dream_team.TeamValidityCheck()
-        dream_team.dream_team = [{'gw_points': 1, 'id': 1, 'name': 'Goalie 1', 'pos': 1, 'price': 1},
-                                                   dict(gw_points=1, id=1, name='Goalie 2', pos=1, price=1)]
+        dream_team.dream_team = []
+        while len(dream_team.dream_team) <= 11:
+            dream_team.dream_team.append(self.test_goalie)
+        # dream_team.dream_team = [{'gw_points': 1, 'id': 1, 'name': 'Goalie 1', 'pos': 1, 'price': 1},
+        #                                            dict(gw_points=1, id=1, name='Goalie 2', pos=1, price=1)]
         dream_team.TeamValidityCheck.goalie_swap(tv_instance)
-        self.assertEqual(len(dream_team.dream_team), 1)
+        self.assertEqual(len(dream_team.dream_team), dream_team.Constants.MAX_GOALKEEPER)
 
     def test_no_goalie(self):
         tv_instance = dream_team.TeamValidityCheck()
@@ -28,36 +36,85 @@ class DreamTeamTests(unittest.TestCase):
                                                    dict(gw_points=1, id=1, name='Goalie 2', pos=1, price=1)]
         dream_team.TeamValidityCheck.goalie_swap(tv_instance)
 
-        self.assertEqual(sum(player['pos'] == 1 for player in dream_team.dream_team), 1)
+        self.assertEqual(sum(player['pos'] == 1 for player in dream_team.dream_team), dream_team.Constants.MIN_GOALKEEPER)
 
     def test_too_many_defenders(self):
         tv_instance = dream_team.TeamValidityCheck()
         dream_team.dream_team = []
         dream_team.provisional_dream_team = []
-        test_defender = {'gw_points': 1, 'id': 1, 'name': 'Test Defender', 'pos': 2, 'price': 1}
-        test_player = {'gw_points': 1, 'id': 1, 'name': 'Test Player', 'pos': 99, 'price': 1}
+
         while len(dream_team.provisional_dream_team) <= 11:
-            dream_team.dream_team.append(test_defender)
-            dream_team.provisional_dream_team.append(test_player)
+            dream_team.dream_team.append(self.test_defender)
+            dream_team.provisional_dream_team.append(self.test_player)
 
         dream_team.TeamValidityCheck.defender_swap(tv_instance)
 
-        self.assertEqual(sum(player['pos'] == 2 for player in dream_team.dream_team), 5)
+        self.assertEqual(sum(player['pos'] == 2 for player in dream_team.dream_team), dream_team.Constants.MAX_DEFENDER)
 
     def test_not_enough_defenders(self):
         tv_instance = dream_team.TeamValidityCheck()
         dream_team.dream_team = []
-        test_defender = {'gw_points': 1, 'id': 1, 'name': 'Test Defender', 'pos': 2, 'price': 1}
-        test_player = {'gw_points': 1, 'id': 1, 'name': 'Test Player', 'pos': 3, 'price': 1}
+        dream_team.provisional_dream_team = []
+
         while len(dream_team.provisional_dream_team) <= 11:
-            dream_team.dream_team.append(test_player)
-            dream_team.provisional_dream_team.append(test_defender)
+            dream_team.dream_team.append(self.test_midfielder)
+            dream_team.provisional_dream_team.append(self.test_defender)
 
         dream_team.TeamValidityCheck.defender_swap(tv_instance)
 
-        self.assertEqual(sum(player['pos'] == 2 for player in dream_team.dream_team), 3)
+        self.assertEqual(sum(player['pos'] == 2 for player in dream_team.dream_team), dream_team.Constants.MIN_DEFENDER)
 
+    def test_not_enough_midfielders(self):
+        tv_instance = dream_team.TeamValidityCheck()
+        dream_team.dream_team = []
+        dream_team.provisional_dream_team = []
 
+        while len(dream_team.provisional_dream_team) <= 11:
+            dream_team.dream_team.append(self.test_defender)
+            dream_team.provisional_dream_team.append(self.test_midfielder)
+
+        dream_team.TeamValidityCheck.midfielder_swap(tv_instance)
+
+        self.assertEqual(sum(player['pos'] == 3 for player in dream_team.dream_team), dream_team.Constants.MIN_MIDFIELDER)
+
+    def test_too_many_midfielders(self):
+        tv_instance = dream_team.TeamValidityCheck()
+        dream_team.dream_team = []
+        dream_team.provisional_dream_team = []
+
+        while len(dream_team.provisional_dream_team) <= 11:
+            dream_team.dream_team.append(self.test_midfielder)
+            dream_team.provisional_dream_team.append(self.test_player)
+
+        dream_team.TeamValidityCheck.midfielder_swap(tv_instance)
+
+        self.assertEqual(sum(player['pos'] == 3 for player in dream_team.dream_team), dream_team.Constants.MAX_MIDFIELDER)
+
+    def test_not_enough_strikers(self):
+        tv_instance = dream_team.TeamValidityCheck()
+        dream_team.dream_team = []
+        dream_team.provisional_dream_team = []
+
+        while len(dream_team.provisional_dream_team) <= 11:
+            dream_team.dream_team.append(self.test_defender)
+            dream_team.provisional_dream_team.append(self.test_striker)
+
+        dream_team.TeamValidityCheck.striker_swap(tv_instance)
+
+        self.assertEqual(sum(player['pos'] == 4 for player in dream_team.dream_team), dream_team.Constants.MIN_STRIKER)
+
+    def test_too_many_strikers(self):
+        tv_instance = dream_team.TeamValidityCheck()
+        dream_team.dream_team = []
+        dream_team.provisional_dream_team = []
+
+        while len(dream_team.provisional_dream_team) <= 11:
+            dream_team.dream_team.append(self.test_striker)
+            dream_team.provisional_dream_team.append(self.test_player)
+
+        dream_team.TeamValidityCheck.striker_swap(tv_instance)
+
+        self.assertEqual(sum(player['pos'] == 4 for player in dream_team.dream_team), dream_team.Constants.MAX_STRIKER)
 
 
 
